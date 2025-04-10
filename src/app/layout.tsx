f321717a46cc1a086ca346aaefd6ac913c6e986c
Amadou-dot <89759636+Amadou-dot@users.noreply.github.com>
2025-04-10 17:56:02 -0600

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import { repositoryName } from "@/prismicio";
+import { PrismicPreview } from "@prismicio/next";
+import clsx from "clsx";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans",
+});
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={clsx(dmSans.variable)}>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        dmSans: ["var(--font-dm-sans)"],
+      },
+    },
+  },
+};
