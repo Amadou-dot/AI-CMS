43dc961001b6d9353576d1e63fa19592befb293a
Amadou-dot <89759636+Amadou-dot@users.noreply.github.com>
2025-04-14 12:59:39 -0600

    Refactor Link button

--- src/app/components/ButtonLink.tsx
+++ src/app/components/ButtonLink.tsx
@@ -8,10 +8,18 @@ export default function ButtonLink({
   return (
     <PrismicNextLink
       className={clsx(
-        "after:bg-opacity-0 after:hover:bg-opacity-15 relative inline-flex h-fit w-fit rounded-full border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 ring-yellow-300 transition-colors outline-none after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-400 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 focus:ring-2 focus:ring-offset-3",
+        "ring-offset-background focus-visible:ring-ring group animate-rainbow text-foreground before:animate-rainbow relative inline-flex h-10 cursor-pointer items-center justify-center rounded-md border-0 bg-[linear-gradient(#fff,#fff),linear-gradient(#fff_50%,rgba(255,255,255,0.6)_80%,rgba(0,0,0,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] bg-[length:200%] [background-clip:padding-box,border-box,border-box] [background-origin:border-box] px-4 py-2 text-sm font-medium whitespace-nowrap transition-transform duration-200 [border:calc(0.08*1rem)_solid_transparent] before:absolute before:bottom-[-20%] before:left-1/2 before:z-[0] before:h-[20%] before:w-[60%] before:-translate-x-1/2 before:bg-[linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))] before:[filter:blur(calc(0.8*1rem))] hover:scale-105 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none active:scale-95 disabled:pointer-events-none disabled:opacity-50 dark:bg-[linear-gradient(#121213,#121213),linear-gradient(#121213_50%,rgba(18,18,19,0.6)_80%,rgba(18,18,19,0)),linear-gradient(90deg,hsl(0,100%,63%),hsl(90,100%,63%),hsl(210,100%,63%),hsl(195,100%,63%),hsl(270,100%,63%))]",
         className,
       )}
       {...restProps}
     />
+
+    // <PrismicNextLink
+    //   className={clsx(
+    //     "after:bg-opacity-0 after:hover:bg-opacity-15 relative inline-flex h-fit w-fit rounded-full border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 ring-yellow-300 transition-colors outline-none after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-400 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 focus:ring-2 focus:ring-offset-3",
+    //     className,
+    //   )}
+    //   {...restProps}
+    // />
   );
 }
